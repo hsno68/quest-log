5b9598b27c9616e79fd721c4243a916fba05f5d5
haiseno <haise.zhao@gmail.com>
2024-01-09 20:34:10 -0800

    Create function to populate quest log table
    Populates the DOM by iterating over the array and its objects'
    properties.
    Iterate over the array to create rows based on number of array items.
    Iterate over the object's properties to create table cells based
    on number of properties.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+const questTable = document.querySelector(".quest-table");
+
 function Quest(name, difficulty, points, releaseDate, status) {
   this.name = name;
   this.difficulty = difficulty;
@@ -10,8 +12,21 @@ function addQuestToQuestLog() {
   questLog.push(new Quest());
 }
 
+function updateQuestLogDisplay() {
+  for (let quest of questLog) {
+    const tableRow = document.createElement("tr");
+    for (let prop in quest) {
+      const tableData = document.createElement("td");
+      tableData.textContent = quest[prop];
+      tableRow.appendChild(tableData);
+    }
+    questTable.appendChild(tableRow);
+  }
+}
+
 const quest1 = new Quest("Cook's Assistant", "Novice", 1, "01/04/2001", true);
 const quest2 = new Quest("Vampyre Slayer", "Intermediate", 3, "01/28/2001", false);
 const quest3 = new Quest("Dragon Slayer I", "Experienced", 2, "09/23/2001", false);
 
 const questLog = [quest1, quest2, quest3];
+updateQuestLogDisplay();
