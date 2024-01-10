3d6ad773c07b0a9b55f98145202236e90ca55f58
haiseno <haise.zhao@gmail.com>
2024-01-09 19:34:04 -0800

    Update array with initial placeholder objects

--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-const questLog = [];
-
 function Quest(name, difficulty, points, releaseDate, completed) {
   this.name = name;
   this.difficulty = difficulty;
@@ -11,3 +9,9 @@ function Quest(name, difficulty, points, releaseDate, completed) {
 function addQuestToQuestLog() {
   questLog.push(new Quest());
 }
+
+const quest1 = new Quest("Cook's Assistant", "Novice", 1, "01/04/2001", true);
+const quest2 = new Quest("Vampyre Slayer", "Intermediate", 3, "01/28/2001", false);
+const quest3 = new Quest("Dragon Slayer I", "Experienced", 2, "09/23/2001", false);
+
+const questLog = [quest1, quest2, quest3];
